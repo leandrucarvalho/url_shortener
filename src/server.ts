39fba0fe1ddffacfbf3604594f8bdc5c185715b9
leandrucarvalho <leandrucs@gmail.com>
2024-03-21 20:30:14 -0300

    final commit

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,90 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { sql } from "./lib/postgres";
+import postgres from "postgres";
+import { redis } from "./lib/redis";
 
-const app = fastify()
+const app = fastify();
 
-app.get("/teste", () => {
-    return "Hello World"
-})
+app.get("/:code", async (request, reply) => {
+  const getLinkSchema = z.object({
+    code: z.string().min(3),
+  });
 
-app.listen({
+  const { code } = getLinkSchema.parse(request.params);
+
+  const result = await sql/*sql*/ `
+    SELECT id, original_url
+      FROM url_shortener 
+    WHERE url_shortener.code = ${code}`;
+
+  if (result.length === 0) {
+    return reply.status(400).send({
+      message: "Link not found!",
+    });
+  }
+  const link = result[0];
+
+  await redis.zIncrBy("metrics", 1, String(link.id));
+
+  return reply.redirect(301, link.original_url);
+});
+
+app.get("/api/links", async () => {
+  const result = await sql/*sql */ `
+  SELECT * FROM url_shortener 
+    ORDER BY created_at DESC`;
+
+  return result;
+});
+
+app.post("/api/links", async (request, reply) => {
+  const { code, url } = z
+    .object({
+      code: z.string().min(3),
+      url: z.string().url(),
+    })
+    .parse(request.body);
+
+  try {
+    const result = await sql/*sql*/ `
+  INSERT INTO url_shortener (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id`;
+
+    const link = result[0];
+
+    return reply.status(201).send({ urlShortenerId: link.id });
+  } catch (error) {
+    if (error instanceof postgres.PostgresError) {
+      if (error.code === "23505") {
+        return reply.status(400).send({ message: "Duplicated code!" });
+      }
+    }
+
+    console.error(error);
+
+    return reply.status(500).send({ message: "Internal error" });
+  }
+});
+
+app.get("/api/metrics", async () => {
+  const result = await redis.zRangeByScoreWithScores("metrics", 0, 50);
+
+  const metrics = result
+    .sort((a, b) => b.score - a.score)
+    .map((item) => {
+      return {
+        shorlLinkId: Number(item.value),
+        clicks: item.score,
+      };
+    });
+
+  return metrics;
+});
+
+app
+  .listen({
     port: 3333,
-}).then(
-    ()=> 
-    console.log("HTTP Server running")
-)
+  })
+  .then(() => console.log("HTTP Server running"));
--- src/setup.ts
+++ src/setup.ts
@@ -1,12 +1,11 @@
 import { sql } from "./lib/postgres";
 
 async function setup() {
-  await sql/* sql */ `
-  CREATE TABLE IF NOT EXISTS url_shortener (
+  await sql/*sql*/ `CREATE TABLE IF NOT EXISTS url_shortener (
     id SERIAL PRIMARY KEY,
     code TEXT UNIQUE,
     original_url TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   )`;
 
   await sql.end();
